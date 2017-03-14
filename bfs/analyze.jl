97f42c0d859595e3fc7452b63703954ebdb93797
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-03-13 23:51:39 -0400

    Update analyze script

--- bfs/analyze.jl
+++ bfs/analyze.jl
@@ -1,12 +1,12 @@
 using Plots, BenchmarkTools, JLD
 
-function analyzec(nthreads, scaleRange, numtrials, plotname)
+function analyzec(nthreads, scaleRange, edgefactor, numtrials, plotname)
     results = zeros(length(nthreads), length(scaleRange), numtrials)
     for (idx, nthread) in enumerate(nthreads)
-        bfstimes = split(readstring(`grep "bfs" output/bfs_bench_$nthread`))
-        for scale in 1:length(scaleRange)
+        for (scaleIdx, scale) in enumerate(scaleRange)
+                bfstimes = split(readstring(`grep "bfs" output/dynograph/dynograph_$(nthread)_$(scale)_$(edgefactor)`))
             for trial in 1:numtrials
-                results[idx, scale, trial] = float(split(split(bfstimes[(scale-1)*numtrials + trial], ",")[end-1], ":")[2])
+                results[idx, scaleIdx, trial] = float(split(split(bfstimes[trial], ",")[end-1], ":")[2])
             end
         end
     end
@@ -18,19 +18,70 @@ function analyzec(nthreads, scaleRange, numtrials, plotname)
     minruntimes
 end
 
-function analyzejulia(nthreads, scaleRange, plotname)
+function analyzestingerwrapper(nthreads, scaleRange, edgefactor, plotname)
     results = zeros(length(nthreads), length(scaleRange))
     for (idx, nthread) in enumerate(nthreads)
-        bfstimes = load("output/threads_bfs_$nthread.jld")
         for (colidx, scale) in enumerate(scaleRange)
-            results[idx, colidx] = minimum(bfstimes["bfs_trial_$(scale)_16"]).time/10^6
+            bfstimes = load("output/stingerwrapper/stingerwrapper_$(nthread)_$(scale)_$(edgefactor).jld")
+            results[idx, colidx] = minimum(bfstimes["bfs_trial"]).time/10^6
         end
     end
     p = plot(scaleRange, results', labels=nthreads', xlabel="Scale", ylabel="log2(Time)",
             markershape=:circle, yscale = :log2, y=10:20, xticks=scaleRange,
-            size=(1280, 720), title="Julia BFS Scaling")
+            size=(1280, 720), title="StingerWrapper BFS Scaling")
     savefig(plotname)
     results
+end
+
+function analyzelg(nthreads, scaleRange, edgefactor, plotname)
+    results = zeros(length(nthreads), length(scaleRange))
+    visitorlg = zeros(1, length(scaleRange))
+    for (idx, nthread) in enumerate(nthreads)
+        for (colidx, scale) in enumerate(scaleRange)
+            bfstimes = load("output/lg/lg_$(nthread)_$(scale)_$(edgefactor).jld")
+            results[idx, colidx] = minimum(bfstimes["bfs_trial"]).time/10^6
+        end
+    end
+    for (idx, scale) in enumerate(scaleRange)
+        try
+            bfstimes = load("output/lg/lg_visitor_$(scale)_$(edgefactor).jld")
+            visitorlg[1, idx] = minimum(bfstimes["bfs_trial"]).time/10^6
+        catch
+            visitorlg[1, idx] = NaN
+        end
+    end
+    p = plot(scaleRange, results', labels=nthreads', xlabel="Scale", ylabel="log2(Time)",
+            markershape=:circle, yscale = :log2, y=10:20, xticks=scaleRange,
+            size=(1280, 720), title="LG BFS Scaling")
+    savefig(plotname)
+    results, visitorlg
+end
+
+function parefficiency(times, scaleRange, nthreads, plotname, title)
+    serial = copy(times[1, :])
+    parefficiency = zeros(size(times))
+    for i=1:size(times, 1)
+        p = nthreads[i]
+        parefficiency[i, :] = serial ./ (times[i,:] * p)
+    end
+    p = plot(nthreads, parefficiency, labels=collect(scaleRange)', xlabel="Scale", ylabel="Parallel Efficiency",
+                markershape=:circle, xticks=nthreads,
+                size=(1280, 720), title="Parallel Efficiency: $title")
+    savefig(plotname)
+    parefficiency
+end
+
+function speedup(times, scaleRange, nthreads, plotname, title)
+    serial = copy(times[1, :])
+    speedups = zeros(size(times))
+    for i=1:size(times, 1)
+        speedups[i, :] = serial ./ (times[i,:])
+    end
+    p = plot(nthreads, speedups, labels=collect(scaleRange)', xlabel="Scale", ylabel="Speedup",
+                markershape=:circle, xticks=nthreads,
+                size=(1280, 720), title="Speedup: $title")
+    savefig(plotname)
+    speedups
 end
 
 function gentable(nthreads, scaleRange, juliaslow)
@@ -48,9 +99,9 @@ function gentable(nthreads, scaleRange, juliaslow)
     end
 end
 
-function genthreadplots(nthreads, scaleRange, c, j)
-    d = [c;j]
-    labels = [["c_$i" for i in nthreads]; ["julia_$i" for i in nthreads]]
+function genthreadplots(nthreads, scaleRange, c, sw, l, visitorlg)
+    d = [c;sw;l;visitorlg]
+    labels = [["stinger_c_$i" for i in nthreads]; ["stinger_jl_$i" for i in nthreads]; ["lg_$i" for i in nthreads]; "lgvisitorbfs"]
     titles = ["$i Threads" for i in nthreads]
     p = plot(scaleRange, d', layout=size(c,1), labels=labels', xlabel="Scale",
     ylabel="Time", markershape=:circle, xticks=scaleRange, title=titles',
@@ -63,12 +114,20 @@ function genthreadplots(nthreads, scaleRange, c, j)
 end
 
 function analyze(nthreads, scaleRange, numtrials)
-    c = analyzec(nthreads, scaleRange, numtrials, "c_thread_bfs_scaling.svg")
-    j = analyzejulia(nthreads, scaleRange, "julia_thread_bfs_scaling.svg")
-    juliaslowdownfactors = j./c
-    genthreadplots(nthreads, scaleRange, c, j)
-    gentable(nthreads, scaleRange, juliaslowdownfactors)
+    c = analyzec(nthreads, scaleRange, 16, numtrials, "stinger_bfs_scaling.svg")
+    j = analyzestingerwrapper(nthreads, scaleRange, 16, "stingerwrapper_bfs_scaling.svg")
+    lg, visitorlg = analyzelg(nthreads, scaleRange, 16, "lg_bfs_scaling.svg")
+    #juliaslowdownfactors = j./c
+    genthreadplots(nthreads, scaleRange, c, j, lg, visitorlg)
+    parefficiency(c, scaleRange, nthreads, "parefficiency_c.svg", "STINGER C - Efficiency")
+    parefficiency(lg, scaleRange, nthreads, "parefficiency_lg.svg", "LG - Efficiency")
+    parefficiency(j, scaleRange, nthreads, "parefficiency_stingerwrapper.svg", "StingerWrapper.jl - Efficiency")
+    speedup(c, scaleRange, nthreads, "speedup_c.svg", "STINGER C - Speedup")
+    speedup(lg, scaleRange, nthreads, "speedup_lg.svg", "LG - Speedup")
+    speedup(j, scaleRange, nthreads, "speedup_stingerwrapper.svg", "StingerWrapper.jl - Speedup")
+    #gentable(nthreads, scaleRange, juliaslowdownfactors)
+    c, j, lg, visitorlg
 end
 
 pyplot()
-analyze([2^i for i in 0:6], 10:20, 3)
+analyze([2^i for i in 0:6], 10:22, 3)
