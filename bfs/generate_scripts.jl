245f4675cc89943e9ab7a34bf129d7123bece52a
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-04-03 17:45:53 -0400

    Swap loops when running scripts

--- bfs/generate_scripts.jl
+++ bfs/generate_scripts.jl
@@ -104,8 +104,8 @@ function runbench(nthreads, scaleRange, edgefactor; qsub=true, useremail="", que
     curdir = dirname(@__FILE__)
     outputdir = joinpath(curdir, "output")
     scriptdir = joinpath(curdir, "scripts")
-    for nthread in nthreads
-        for scale in scaleRange
+    for scale in scaleRange
+        for nthread in nthreads
             lgscript = lg_bench_script(scale, edgefactor, joinpath(outputdir, "lg", "lg_$(nthread)_$(scale)_$(edgefactor).jld"), nthread)
             stingerwrapperscript = stingerwrapper_bench_script(scale, edgefactor, joinpath(outputdir, "stingerwrapper", "stingerwrapper_$(nthread)_$(scale)_$(edgefactor).jld"), nthread)
             dynographscript = dynograph_bench_script(scale, edgefactor, joinpath(outputdir, "dynograph", "dynograph_$(nthread)_$(scale)_$(edgefactor)"), nthread)
